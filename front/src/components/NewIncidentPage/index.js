88e6218c6d751007e28f0719e2536ff3d936a425
Sebby <schabrol@lafourchette.com>
2021-07-21 14:01:28 +0200

    [FRONT] structure form by a table

--- front/src/components/NewIncidentPage/index.js
+++ front/src/components/NewIncidentPage/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+
 import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Button from '@material-ui/core/Button';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 import DateFnsUtils from '@date-io/date-fns';
 import {
@@ -14,6 +25,9 @@ import './style.css';
 const incidentTypeList = ['Conditionnement', 'Livraison', 'Point Relais', 'Produit', 'Client'];
 const incidentOriginList = ['Retard', 'Casier manquant', 'Temperature'];
 const resolutionList = ['Email', 'Appel téléphonique', 'Remboursement partiel', 'Remboursmeent total'];
+const relayPoint = ['Biocoop', 'cafe de al gare', 'Biocoop da cote', 'Biocoop den bas'];
+
+// TODO display flashMessage after submit
 
 class NewIncidentPage extends React.Component {
   constructor(props) {
@@ -21,8 +35,12 @@ class NewIncidentPage extends React.Component {
     this.state = {};
   }
   
+  // Incident type change
+  handleTypeChange(event, value) {
+    console.log('---- TYPE : ', event, '-------', value);
+  }
   render() {
-    // const [selectedDate, handleDateChange] = this.state;
+    // const [selectedDate, handleTypeChange] = this.state;
 
     return (
       <div className="newIncidentWrapper">
@@ -31,43 +49,80 @@ class NewIncidentPage extends React.Component {
           <span className="title">Création d'un nouvel incident</span>
         </div>
         
-          <div>
+          <div className="form">
             <form>
               <FormControl>
-                <TextField id="outlined-basic" label="Email" variant="outlined" name="email[]"/>
-                <TextField id="outlined-basic" label="Point Relais" variant="outlined" name="relayPoint"/>
-                <Autocomplete
-                  id="combo-box-demo"
-                  options={incidentTypeList}
-                  getOptionLabel={(label) => label}
-                  style={{ width: 300 }}
-                  renderInput={(params) => <TextField {...params} label="Type d'incident" variant="outlined" />}
-                />
-                <Autocomplete
-                  id="combo-box-demo"
-                  options={incidentOriginList}
-                  getOptionLabel={(label) => label}
-                  style={{ width: 300 }}
-                  renderInput={(params) => <TextField {...params} label="Cause d'incident" variant="outlined" />}
-                />
-                <Autocomplete
-                  id="combo-box-demo"
-                  options={resolutionList}
-                  getOptionLabel={(label) => label}
-                  style={{ width: 300 }}
-                  renderInput={(params) => <TextField {...params} label="Résolution incident" variant="outlined" />}
-                />
-                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                  <KeyboardDatePicker
-                    clearable
-                    // value={selectedDate}
-                    placeholder="Date"
-                    // onChange={date => handleDateChange(date)}
-                    minDate={new Date()}
-                    format="MM/dd/yyyy"
-                    name="Date"
-                  />
-                </MuiPickersUtilsProvider>
+                <TableContainer component={Paper}>
+                  <Table>
+                    <TableBody>
+                      
+                      <TableRow>
+                        <TableCell>
+                          <div className="emailArea">
+                            <span className="email">
+                              <TextField type="email" label="Email" variant="outlined" name="email[]"/>
+                            </span>
+                            <span className="addEmail">
+                              <AddCircleOutlineIcon color="secondary" fontSize="large"/>
+                            </span>
+                          </div>
+                        </TableCell>
+                        <TableCell>
+                          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                            <KeyboardDatePicker
+                              clearable
+                              // value={selectedDate}
+                              placeholder="Date"
+                              // onChange={date => handleDateChange(date)}
+                              minDate={new Date()}
+                              format="MM/dd/yyyy"
+                              name="Date"
+                            />
+                          </MuiPickersUtilsProvider>
+                        </TableCell>
+                        <TableCell>
+                          <TextField label="Point Relais" variant="outlined" name="relayPoint"/>
+                        </TableCell>
+                      </TableRow>
+                      
+                      <TableRow>
+                        <TableCell>
+                          <Autocomplete
+                            onInputChange={(event, value) => this.handleTypeChange(event, value)}
+                            options={incidentTypeList}
+                            getOptionLabel={(label) => label}
+                            style={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Type d'incident" variant="outlined" />}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <Autocomplete
+                            disabled
+                            getOptionLabel={(label) => label}
+                            style={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Cause d'incident" variant="outlined" />}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <Autocomplete
+                            disabled
+                            getOptionLabel={(label) => label}
+                            style={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Résolution incident" variant="outlined" />}
+                          />
+                        </TableCell>
+                      </TableRow>
+                      
+                      <TableRow>
+                        <TableCell colSpan={3}>
+                          <Button type="submit" variant="contained" color="primary">Sauvegarder</Button>
+                        </TableCell>
+                      </TableRow>
+                      
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+                
               </FormControl>
             </form>
           </div>
