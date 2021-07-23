5cc6ed62c66812ef82a4cb45d9b06b2c94a3b374
Sebby <schabrol@lafourchette.com>
2021-07-23 14:56:40 +0200

    [FRONT] use axios to HTTP requests

--- front/src/components/ListIncidentPage/api.js
+++ front/src/components/ListIncidentPage/api.js
@@ -1,15 +1,15 @@
 import React from "react";
+import axios from 'axios';
 
 /**
  * Get data from API.
  * Can be handle data/error from API, before render in component
  */
 const getData = () =>
-  fetch('http://localhost:3001/api/incidents')
-    .then(res => res.json())
+  axios.get('http://localhost:3001/api/incidents')
     .then(
-      (result) => result.data,
-      (error) =>  {
+      result => result.data.data,
+      error =>  {
         console.log('--- ERROR : ', error);
         return null;
       }
