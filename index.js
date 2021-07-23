3e1ead7da74e2f48959f948af2b4e536f06b41c3
Sebby <schabrol@lafourchette.com>
2021-07-23 23:25:37 +0200

    [API] creatIncident routes

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import { config } from 'dotenv';
 // import fishSchemaInput from './lib/schemas/fishInput.js';
 import fishHandler from './lib/handlers/fish.js';
-import incidentHandler from './lib/handlers/incident.js';
+import { incidentListHandler, createIncidentHandler } from './lib/handlers/incident.js';
 
-// get env config
+// get config
 config();
+const PORT = process.env.PORT || 3001;
 
 //  APP
 const app = express();
-const PORT = process.env.PORT || 3001;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 // TODO : move routes to specific files
 app.get('/api/fishs', fishHandler);
-app.get('/api/incidents', incidentHandler);
+app.get('/api/incidents', incidentListHandler);
+app.post('/api/createIncident', createIncidentHandler);
 
 app.listen(PORT);
